f6f093ef5db72912657dc9b9fb9523f637a50275
Rahel Knüsel <rahel.knuesel@hotmail.ch>
2023-12-11 10:23:12 +0100

    changed html and js structure and changed div, id-names and function because my structure did not work

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const container = document.getElementById("container");
-document.querySelector("#request-btn").addEventListener("click", getData);
+document.getElementById("getWeatherBtn").addEventListener("click", () => {
+  const city = document.getElementById("cityInput").value;
+  getWeather(city);
+});
 
-const test = 5;
-async function getData() {
+async function getWeather(city) {
   try {
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?q=Romanshorn&appid=f56f940d59573a50c91e8967c8559adf&lang=de&units=metric";
-    const response = await fetch(url);
+    const apiKey = "f56f940d59573a50c91e8967c8559adf"; // Set your API key here
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=de&units=metric`
+    );
     const data = await response.json();
-    console.log("my data:", data.name);
-    let newElement = document.createElement("p");
-    container.textContent = "";
-    newElement.textContent = data.name;
-    container.appendChild(newElement);
-  } catch (err) {
-    console.log(err);
+    displayWeather(data);
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Wetterdaten", error);
   }
 }
 
-getData();
+function displayWeather(data) {
+  const weatherDiv = document.getElementById("weatherResult");
+  weatherDiv.innerHTML = `
+      <h2>Wetter in ${data.name}</h2>
+      <p>Temperatur: ${data.main.temp} °C</p>
+      <p>Zustand: ${data.weather[0].description}</p>
+  `;
+}
+
+// Fragen: let ist verànderlich, const ist konstant, (die Eingaben veràndern sich die Daten jedoch nicht?)
+// weatherDiv.innerHTML (konstante aufrufen, hol mir aus dem html den h2, p, p Tag, fège die Daten ein)
+//syntax: weatherDiv.innerHTML = `
+//     <h2>Wetter in ${data.name}</h2>
+//     <p>Temperatur: ${data.main.temp} °C</p>
+//     <p>Zustand: ${data.weather[0].description}</p>
+// `;
